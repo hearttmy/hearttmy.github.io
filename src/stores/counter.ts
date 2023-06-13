77ee1c066d0f13314478468dccf68cdd51d9a1eb
yaojiu <yaojiu@yottastudios.com>
2023-06-13 15:44:45 +0800

    AP test

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -6,6 +6,10 @@ export const useCounterStore = defineStore('counter', () => {
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+    console.log(window.AP)
+    // AP.getContext((contextData) => {
+    //   console.log("Object: context", contextData);
+    // });
   }
 
   return { count, doubleCount, increment }

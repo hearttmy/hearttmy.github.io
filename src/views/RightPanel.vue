267a076eb7a37835f1a5c934d2261828a0f06b10
yaojiu <yaojiu@yottastudios.com>
2023-06-14 13:27:58 +0800

    test

--- src/views/RightPanel.vue
+++ src/views/RightPanel.vue
@@ -1,21 +1,26 @@
 <script setup>
-console.log('AP')
-console.log(AP)
-AP.getContext(function(contextData) {
-  console.log("Object: context", contextData);
-});
+import {ElButton} from 'element-plus'
 
-AP.editor.getString(function(stringData) {
-    console.log("Object: source string", stringData);
-});
+const test = () => {
+  console.log('AP')
+  console.log(AP)
+  AP.getContext(function(contextData) {
+    console.log("Object: context", contextData);
+  });
 
-AP.editor.getTranslations(function(translationsData) {
-    console.log("Array: translation objects", translationsData);
-});
+  AP.editor.getString(function(stringData) {
+      console.log("Object: source string", stringData);
+  });
+
+  AP.editor.getTranslations(function(translationsData) {
+      console.log("Array: translation objects", translationsData);
+  });
+}
 </script>
 
 <template>
   <main>
     123123123
+    <ElButton @click="test">test</ElButton>
   </main>
 </template>
